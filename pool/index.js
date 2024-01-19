56b0701dc06a57ee3a4d2d0834e962601236dbba
illonion <zhou.lawrence.yz@gmail.com>
2024-01-19 20:22:54 +1100

    Import same chat as showcase into the mappool screen

---
+++ pool/index.js
@@ -0,0 +1,15 @@
+// Sponsor animations
+const sponsors = document.getElementById("sponsors")
+let currentSponsorIndex = 0
+function sponsorAnimations() {
+    setInterval(async () => {
+        if (sponsors.childElementCount <= currentSponsorIndex) currentSponsorIndex = 0
+        const currentSponsor = sponsors.children[currentSponsorIndex]
+        currentSponsor.style.opacity = 1
+        await sleep(5000)
+        currentSponsor.style.opacity = 0
+        await sleep(1000)
+        currentSponsorIndex++
+    }, 6100)
+}
+sponsorAnimations()
