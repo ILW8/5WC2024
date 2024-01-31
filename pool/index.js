89ab2004ff5af00799f104e3dac8af8c8a847296
illonion <zhou.lawrence.yz@gmail.com>
2024-02-01 00:20:15 +1100

    Move sponsor animations to shared folder
    - This code will be used for a bunch of different HTML files

--- pool/index.js
+++ pool/index.js
@@ -331,26 +331,3 @@ socket.onmessage = async (event) => {
         chatContainer.scrollTop = chatContainer.scrollHeight;
     }
 }
-
-// Sponsor animations
-const sleep = ms => new Promise(res => setTimeout(res, ms))
-const sponsors = document.getElementById("sponsors")
-let currentSponsorIndex = 0
-let intervalId
-function startInterval() {
-    if (intervalId) clearInterval(intervalId)
-  
-    intervalId = setInterval(async () => {
-        if (sponsors.childElementCount <= currentSponsorIndex) currentSponsorIndex = 0
-        const currentSponsor = sponsors.children[currentSponsorIndex]
-        currentSponsor.style.opacity = 1
-        await sleep(5000)
-        currentSponsor.style.opacity = 0
-        await sleep(1000)
-        currentSponsorIndex++
-    }, 6000)
-}
-startInterval();
-setInterval(function() {
-    startInterval()
-}, 120000)
--- showcase/index.js
+++ showcase/index.js
@@ -337,28 +337,7 @@ function setBPMStats() {
     }
 }
 
-// Sponsor animations
-const sponsors = document.getElementById("sponsors")
-let currentSponsorIndex = 0
-let intervalId
-function startInterval() {
-    if (intervalId) clearInterval(intervalId)
-  
-    intervalId = setInterval(async () => {
-        if (sponsors.childElementCount <= currentSponsorIndex) currentSponsorIndex = 0
-        const currentSponsor = sponsors.children[currentSponsorIndex]
-        currentSponsor.style.opacity = 1
-        await sleep(5000)
-        currentSponsor.style.opacity = 0
-        await sleep(1000)
-        currentSponsorIndex++
-    }, 6000)
-}
-startInterval();
-setInterval(function() {
-    startInterval()
-}, 120000)
-
+// Configs are for strain graphs
 let config = {
 	type: 'line',
 	data: {
